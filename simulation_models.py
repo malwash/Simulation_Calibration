e6634716e0dca41b20f034a34f823b8ef0159438
Mostafa Alwash <malwash@gmail.com>
2022-03-19 18:15:55 +0100

    Removed crossvalidation and used sklearn.accuracy_score

--- simulation_models.py
+++ simulation_models.py
@@ -12,70 +12,77 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.naive_bayes import GaussianNB, CategoricalNB, BernoulliNB, MultinomialNB, ComplementNB
 from sklearn import svm
 from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import accuracy_score
 
 def decision_tree(x_train, y_train, x_test, y_test):
     clf = DecisionTreeClassifier(criterion='gini')
     clf = clf.fit(x_train, y_train)
-    #y_pred = clf.predict(x_test)
-    #return metrics.accuracy_score(y_test,y_pred)
-    return cross_val_score(clf, x_test, y_test, cv=10)
+    y_pred = clf.predict(x_test)
+    #print(accuracy_score(y_test, y_pred))
+    return accuracy_score(y_test, y_pred)
 
 def decision_tree_entropy(x_train, y_train, x_test, y_test):
     clf = DecisionTreeClassifier(criterion='entropy')
     clf = clf.fit(x_train, y_train)
-    #y_pred = clf.predict(x_test)
-    #return metrics.accuracy_score(y_test,y_pred)
-    return cross_val_score(clf, x_test, y_test, cv=10)
+    y_pred = clf.predict(x_test)
+    # print(accuracy_score(y_test, y_pred))
+    return accuracy_score(y_test, y_pred)
 
 def random_forest(x_train, y_train, x_test, y_test):
     rf = RandomForestClassifier(criterion='gini')
     rf = rf.fit(x_train, y_train)
-    #y_pred = rf.predict(x_test)
-    #return metrics.accuracy_score(y_test,y_pred)
-    return cross_val_score(rf, x_test, y_test, cv=10)
+    y_pred = rf.predict(x_test)
+    # print(accuracy_score(y_test, y_pred))
+    return accuracy_score(y_test, y_pred)
 
 def random_forest_entropy(x_train, y_train, x_test, y_test):
     rf = RandomForestClassifier(criterion='entropy')
     rf = rf.fit(x_train, y_train)
-    #y_pred = rf.predict(x_test)
-    #return metrics.accuracy_score(y_test,y_pred)
-    return cross_val_score(rf, x_test, y_test, cv=10)
+    y_pred = rf.predict(x_test)
+    # print(accuracy_score(y_test, y_pred))
+    return accuracy_score(y_test, y_pred)
 
 def logistic_regression(x_train, y_train, x_test, y_test):
     lr = LogisticRegression(penalty='none')
     lr = lr.fit(x_train, y_train)
-    #y_pred = lr.predict(x_test)
-    return cross_val_score(lr, x_test, y_test, cv=10)
+    y_pred = lr.predict(x_test)
+    # print(accuracy_score(y_test, y_pred))
+    return accuracy_score(y_test, y_pred)
 
 def logistic_regression_l1(x_train, y_train, x_test, y_test):
     lr = LogisticRegression(penalty='l1', solver='liblinear', l1_ratio=1)
     lr = lr.fit(x_train, y_train)
-    #y_pred = lr.predict(x_test)
-    return cross_val_score(lr, x_test, y_test, cv=10)
+    y_pred = lr.predict(x_test)
+    # print(accuracy_score(y_test, y_pred))
+    return accuracy_score(y_test, y_pred)
 
 def logistic_regression_l2(x_train, y_train, x_test, y_test):
     lr = LogisticRegression(penalty='l2')
     lr = lr.fit(x_train, y_train)
-    #y_pred = lr.predict(x_test)
-    return cross_val_score(lr, x_test, y_test, cv=10)
+    y_pred = lr.predict(x_test)
+    # print(accuracy_score(y_test, y_pred))
+    return accuracy_score(y_test, y_pred)
 
 def logistic_regression_elastic(x_train, y_train, x_test, y_test):
     lr = LogisticRegression(penalty='elasticnet', solver='saga', l1_ratio=0.5)
     lr = lr.fit(x_train, y_train)
-    #y_pred = lr.predict(x_test)
-    return cross_val_score(lr, x_test, y_test, cv=10)
+    y_pred = lr.predict(x_test)
+    # print(accuracy_score(y_test, y_pred))
+    return accuracy_score(y_test, y_pred)
 
 def naive_bayes(x_train, y_train, x_test, y_test):
     gnb = BernoulliNB()
     gnb = gnb.fit(x_train, y_train)
-    #y_pred = gnb.predict(x_test)
-    return cross_val_score(gnb, x_test, y_test, cv=10)
+    y_pred = gnb.predict(x_test)
+    # print(accuracy_score(y_test, y_pred))
+    return accuracy_score(y_test, y_pred)
 
 def naive_bayes_gaussian(x_train, y_train, x_test, y_test):
     gnb = GaussianNB()
     gnb = gnb.fit(x_train, y_train)
-    #y_pred = gnb.predict(x_test)
-    return cross_val_score(gnb, x_test, y_test, cv=10)
+    y_pred = gnb.predict(x_test)
+    # print(accuracy_score(y_test, y_pred))
+    return accuracy_score(y_test, y_pred)
 
 def naive_bayes_multinomial(x_train, y_train, x_test, y_test):
     min_max_scaler = MinMaxScaler()
@@ -83,8 +90,9 @@ def naive_bayes_multinomial(x_train, y_train, x_test, y_test):
     X_test_minmax = min_max_scaler.transform(x_test)
     gnb = MultinomialNB()
     gnb = gnb.fit(X_train_minmax, y_train)
-    #y_pred = gnb.predict(X_test_minmax)
-    return cross_val_score(gnb, X_test_minmax, y_test, cv=10)
+    y_pred = gnb.predict(X_test_minmax)
+    # print(accuracy_score(y_test, y_pred))
+    return accuracy_score(y_test, y_pred)
 
 def naive_bayes_complement(x_train, y_train, x_test, y_test):
     min_max_scaler = MinMaxScaler()
@@ -92,51 +100,58 @@ def naive_bayes_complement(x_train, y_train, x_test, y_test):
     X_test_minmax = min_max_scaler.transform(x_test)
     gnb = ComplementNB() #ValueError: Negative values in data passed to ComplementNB (input X)
     gnb = gnb.fit(X_train_minmax, y_train)
-    #y_pred = gnb.predict(X_test_minmax)
-    return cross_val_score(gnb, X_test_minmax, y_test, cv=10)
+    y_pred = gnb.predict(X_test_minmax)
+    # print(accuracy_score(y_test, y_pred))
+    return accuracy_score(y_test, y_pred)
 
 def support_vector_machines(x_train, y_train, x_test, y_test):
     clf = svm.SVC(kernel="sigmoid")
     clf = clf.fit(x_train, y_train)
-    #y_pred = clf.predict(x_test)
-    return cross_val_score(clf, x_test, y_test, cv=10)
+    y_pred = clf.predict(x_test)
+    # print(accuracy_score(y_test, y_pred))
+    return accuracy_score(y_test, y_pred)
 
 def support_vector_machines_linear(x_train, y_train, x_test, y_test):
     clf = svm.SVC(kernel="linear")
     clf = clf.fit(x_train, y_train)
-    #y_pred = clf.predict(x_test)
-    return cross_val_score(clf, x_test, y_test, cv=10)
+    y_pred = clf.predict(x_test)
+    # print(accuracy_score(y_test, y_pred))
+    return accuracy_score(y_test, y_pred)
 
 def support_vector_machines_poly(x_train, y_train, x_test, y_test):
     clf = svm.SVC(kernel="poly")
     clf = clf.fit(x_train, y_train)
-    #y_pred = clf.predict(x_test)
-    return cross_val_score(clf, x_test, y_test, cv=10)
+    y_pred = clf.predict(x_test)
+    # print(accuracy_score(y_test, y_pred))
+    return accuracy_score(y_test, y_pred)
 
 def support_vector_machines_rbf(x_train, y_train, x_test, y_test):
     clf = svm.SVC(kernel="rbf")
     clf = clf.fit(x_train, y_train)
-    #y_pred = clf.predict(x_test)
-    return cross_val_score(clf, x_test, y_test, cv=10)
+    y_pred = clf.predict(x_test)
+    # print(accuracy_score(y_test, y_pred))
+    return accuracy_score(y_test, y_pred)
 
 def support_vector_machines_precomputed(x_train, y_train, x_test, y_test):
     clf = svm.SVC(kernel="precomputed")
     clf = clf.fit(x_train, y_train)
-    #y_pred = clf.predict(x_test)
-    return cross_val_score(clf, x_test, y_test, cv=10)
+    y_pred = clf.predict(x_test)
+    # print(accuracy_score(y_test, y_pred))
+    return accuracy_score(y_test, y_pred)
 
 def k_nearest_neighbor(x_train, y_train, x_test, y_test):
     clf = KNeighborsClassifier(weights='uniform')
     clf = clf.fit(x_train, y_train)
-    #y_pred = clf.predict(x_test)
-    return cross_val_score(clf, x_test, y_test, cv=10)
+    y_pred = clf.predict(x_test)
+    # print(accuracy_score(y_test, y_pred))
+    return accuracy_score(y_test, y_pred)
 
 def k_nearest_neighbor_distance(x_train, y_train, x_test, y_test):
     clf = KNeighborsClassifier(weights='distance')
     clf = clf.fit(x_train, y_train)
-    #y_pred = clf.predict(x_test)
-    #return metrics.accuracy_score(y_test, y_pred)
-    return cross_val_score(clf, x_test, y_test, cv=10)
+    y_pred = clf.predict(x_test)
+    # print(accuracy_score(y_test, y_pred))
+    return accuracy_score(y_test, y_pred)
 
 #def decision_tree():
 #    clf = DecisionTreeClassifier(random_state=0)
